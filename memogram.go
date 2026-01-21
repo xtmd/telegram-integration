bd802596c36793fc37e13ac76757c0d0eb8d4290
X. <127165438+xtmd@users.noreply.github.com>
2026-01-21 14:04:09 +0800

    Remove session check before memo listing
    Removed current session validation before listing memos.

--- memogram.go
+++ memogram.go
@@ -461,14 +461,7 @@ func (s *Service) searchHandler(ctx context.Context, b *bot.Bot, m *models.Updat
 	searchString := strings.TrimPrefix(m.Message.Text, "/search ")
 	accessToken, _ := s.store.GetUserAccessToken(userID)
 	ctx = metadata.NewOutgoingContext(ctx, metadata.Pairs("Authorization", fmt.Sprintf("Bearer %s", accessToken)))
-	currentSessionResponse, err := s.client.AuthService.GetCurrentSession(ctx, &v1pb.GetCurrentSessionRequest{})
-	if err != nil {
-		b.SendMessage(ctx, &bot.SendMessageParams{
-			ChatID: m.Message.Chat.ID,
-			Text:   "Invalid access token",
-		})
-		return
-	}
+
 	results, err := s.client.MemoService.ListMemos(ctx, &v1pb.ListMemosRequest{
 		PageSize: 10,
 		Filter:   fmt.Sprintf("content.contains('%s')", searchString),
